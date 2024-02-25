a2c6125d64fe64573faf32e0ba1671883adaaf81
devadmin <devadmin@domain.tvtworld.com>
2024-02-25 18:31:49 +0530

    Remove unnecessary files

--- functiontype.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-function palindrom(str) {
-    let reverse = str.split("").reverse().join("");
-    return reverse === str;
-}
-console.log(palindrom("12321"));
--- functiontype.ts
+++
@@ -1,5 +0,0 @@
-function palindrom(str:string):boolean{
-    let reverse = str.split("").reverse().join("");
-    return reverse===str;
-}
-console.log(palindrom("12321"));
